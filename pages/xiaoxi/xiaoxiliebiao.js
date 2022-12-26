7946fb08a7779120b40ce7c5eefcc6ba99e00a17
shiruiqiang <535285745@qq.com>
2022-12-26 17:19:35 +0800

    消息列表页

---
+++ pages/xiaoxi/xiaoxiliebiao.js
@@ -0,0 +1,70 @@
+// pages/xiaoxi/xiaoxiliebiao.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    curpage: "xiaoxi",
+    usersList:[{name:"童俊豪",src:"http://www.tuling123.com/resources/web/v4/img/personalCen/icon40.png",time:"2022-12-25 16:15"},
+      {name:"童俊豪",src:"http://www.tuling123.com/resources/web/v4/img/personalCen/icon40.png",time:"2022-12-25 16:15"},
+      {name:"童俊豪",src:"http://www.tuling123.com/resources/web/v4/img/personalCen/icon40.png",time:"2022-12-25 16:15"}]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
